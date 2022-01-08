e81d48ad54648343fb716c3d979d172a053f4dd3
aayushave <31796139+aayushave@users.noreply.github.com>
2022-01-08 20:49:55 +0530

    Review Changes

--- src/components/Landing/Centered.js
+++ src/components/Landing/Centered.js
@@ -5,7 +5,7 @@ import './../../assets/css/custom.css'
 const Centered = () => {
     return (
         <div className="centered">
-            <h1>LOREM IPSUM TITLE GOES HERE</h1>
+            <span className="centered-style">LOREM IPSUM TITLE GOES HERE</span>
         </div>
     )
 }
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -50,14 +50,14 @@ const Portfolio = () => {
                 </div>
 
                 <div className="row " style={{ padding: '0 0 20px 0' }}>
-                    <div className="col-sm-6 col-lg-4 ">
+                    <div className="col-sm-6 col-lg-3 col-xl-3 ">
                         <div className='portfolio-proj'>
                             <p>
                                 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English.
                             </p>
                         </div>
                     </div>
-                    <div className="col-sm col-6 col-sm-6 col-md-6 col-lg-3 col-xl-2 fadeInUp" >
+                    <div className="col-sm col-6 col-sm-6 col-md-6 col-lg-3 col-xl-3 fadeInUp" >
                         <div className='portfolio-project'>
                             <div style={{ marginLeft: '10px' }}>
                                 <FadeInWhenVisible>
@@ -68,7 +68,7 @@ const Portfolio = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-sm col-6 col-sm-6 col-md-6 col-lg-3 col-xl-2 fadeInUp">
+                    <div className="col-sm col-6 col-sm-6 col-md-6 col-lg-3 col-xl-3 fadeInUp">
                         <div className='portfolio-project-x'>
                             <div style={{ marginLeft: '10px' }}>
                             <FadeInWhenVisible>
@@ -82,7 +82,7 @@ const Portfolio = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-sm col-6 col-sm-6 col-md-6 col-lg-3 col-xl-2 fadeInUp">
+                    <div className="col-sm col-6 col-sm-6 col-md-6 col-lg-3 col-xl-3 fadeInUp">
                         <div className='portfolio-project-x'>
                             <div style={{ marginLeft: '10px' }}>
                             <FadeInWhenVisible>
