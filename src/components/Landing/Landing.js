8802e2b5973e27f60ecaf6603a0433414a7d19db
aayushave <31796139+aayushave@users.noreply.github.com>
2022-01-15 14:23:37 +0530

    Added Candelier| Table Motions

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,24 +1,11 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
-import BgImage from './../../assets/images/bg-image.jpg'
 import Header from '../Header';
 import Centered from './Centered';
-const useStyles = makeStyles((theme) => ({
-root:{
-    minHeight:'100vh',
-    backgroundImage:`url(${BgImage})`,
-    backgroundRepeat: 'no-repeat',
-    backgroundSize:'cover',
-    backgroundPosition:'center'
-}
-
-}))
-
+import './../../assets/css/custom.css'
 
 const Landing = () => {
-const classes = useStyles();
     return (
-        <div className={classes.root}>
+        <div className='landing-image'>
          <Header />
         <Centered/>
         </div>
